b8c0a5c9c3524220b4de79e6215b3654cf90c9e2
Kevin Cui <k.cui1234@gmail.com>
2024-12-13 16:18:09 -0500

    integrated phase 3 to workflow (mutation testing)

--- translation/translate.py
+++ translation/translate.py
@@ -112,6 +112,10 @@ def detect_errors_in_test_output(test_output):
                     )
                     errors.append(error_detail)
 
+            if not errors and "ERROR" in test_output_content and "100%" not in test_output_content:
+                #likely a collection error
+                errors = [test_output_content]
+
             return errors
     except Exception as e:
         print(f'Error reading file: {e}')
@@ -200,8 +204,8 @@ def check_test(test_files, output_dir, run_no, response_history, error_path):
     else:
         return []  # No errors
 
-
-def phase_1(files, input_dir, output_dir, execute_script_path, error_path, num_of_retries):
+#p1
+def source_code_translate(files, input_dir, output_dir, execute_script_path, error_path, num_of_retries):
     run_no = 1
     os.makedirs(output_dir + f'/run_{run_no}/')
     response_history = save_outputs(files, input_dir, output_dir+f'/run_{run_no}/', isPhase2=False)
@@ -210,7 +214,6 @@ def phase_1(files, input_dir, output_dir, execute_script_path, error_path, num_o
         if errors:
             print("Errors detected in translated source code, initiating step 1a (feedback loop)")
         while errors:
-            # print("ENTERING FEEDBACK LOOP")
             if run_no == num_of_retries:
                 break
             run_no += 1
@@ -224,13 +227,14 @@ def phase_1(files, input_dir, output_dir, execute_script_path, error_path, num_o
             print(f'Source code successfully translated after {run_no-1} feedback iterations.')
         else:
             print(f'Max iteration count reached. Number of remaining errors in source code: {len(errors)}')
-            # print("WOMP WOMP :(")
-    return response_history, errors
+    return response_history, errors, run_no
 
-def phase_2(output_dir, error_path, testFiles, input_dir_test, history, num_of_retries):
+#p2
+def source_test_translate(output_dir, error_path, testFiles, input_dir_test, history, num_of_retries):
     test_run_no = 1
     os.makedirs(output_dir + f'/test_run_{test_run_no}')
     response_history = save_outputs(testFiles, input_dir_test, output_dir+f'/test_run_{test_run_no}',True, history)
+    success = False
     if num_of_retries > 0:
         errors = check_test(testFiles, output_dir, test_run_no, response_history, error_path)
         if errors:
@@ -247,25 +251,77 @@ def phase_2(output_dir, error_path, testFiles, input_dir_test, history, num_of_r
 
         if not errors:
             print(f'Test code successfully translated after {test_run_no-1} feedback iterations.')
+            success = True
         else:
             print(f'Max iteration count reached. Number of remaining errors in source code: {len(errors)}')
             # print("WOMP WOMP :(")
-    return response_history
+    return response_history, success, test_run_no
+
+#p3
+def mutation_test(files, testFiles, output_dir, run_no, test_run_no, error_path):
+    #first copy all necessary files into temp
+    for testFile in testFiles:
+        shutil.copyfile(output_dir + f'/test_run_{test_run_no}/' + testFile + '.py', 'temp/' + testFile + '.py')
+    num_fn_iterations = 0
+    for i in range(1,run_no):
+        for file in files:
+            shutil.copyfile(output_dir + f'/run_{i}/' + file + '.py', 'temp/' + file + '.py')
+
+        #running mutation testing (should fail at least once for each)
+        num_fp_files = 0
+        for test_file_name in testFiles:
+            test_file_path = os.path.join('temp/', f'{test_file_name}.py')
+            try:
+                # Open the error_path to write combined stdout and stderr
+                with open(error_path, 'w') as error_file:
+                    result = subprocess.run(
+                        ['pytest', test_file_path],  # Using pytest to run the test
+                        stdout=subprocess.PIPE,      # Capture stdout in result
+                        stderr=subprocess.STDOUT,    # Merge stderr with stdout
+                        text=True,                   # Return output as text (not bytes)
+                        check=False                  # Don't raise an exception on non-zero exit status
+                    )
+                    error_file.write(result.stdout)
+
+                errors = detect_errors_in_test_output(error_path)
+                if not errors:
+                    print(f'Mutation test failed (actual test passed) for {test_file_name} in run_no: {i}')
+                if errors:
+                    num_fp_files += 1
+            except subprocess.CalledProcessError as e:
+                print(f"Error occurred while running the test: {e}")
+
+        # After all tests are run, return the accumulated errors
+        if num_fp_files == 0: #BAD
+            print(f'Mutation testing passed for run_no: {i}. Tests are not strong enough.')
+            num_fn_iterations += 1
+        num_fp_files = 0
+    if num_fn_iterations == 0: #GOOD!
+        print(f'Mutation testing successfully passed for all runs from run numbers 1 to {run_no}.')
+
 
 def llm_translate(files, input_dir, output_dir, execute_script_path, error_path, testFiles, input_dir_test, testing_phase_2=True, num_of_retries=DEFAULT_NUM_RETRIES):
-    print("**** STARTING PHASE 1 ****")
+    print("**** STARTING PHASE 1: Source Code Translation ****")
     os.makedirs(output_dir)
-    response_history, errors = phase_1(files, input_dir, output_dir, execute_script_path, error_path, num_of_retries)
+    response_history, errors, run_no = source_code_translate(files, input_dir, output_dir, execute_script_path, error_path, num_of_retries)
+    print("**** PHASE 1 COMPLETE ****")
     print("\n")
-    print("**** PHASE 1 COMPLETE, STARTING PHASE 2 ****")
+    print("**** STARTING PHASE 2: Source Test Translation ****")
     #if there are errors, we should only run the test generation once because the source code had issues
     if errors:
         num_of_retries = 0
-    response_history = phase_2(output_dir, error_path, testFiles, input_dir_test, response_history, num_of_retries)
-    print("**** PHASE 2 COMPLETE****")
+    response_history, success, test_run_no = source_test_translate(output_dir, error_path, testFiles, input_dir_test, response_history, num_of_retries)
+    print("**** PHASE 2 COMPLETE ****")
+    print("\n")
+    if success:
+        print("**** INITIATING PHASE 3: Translated Test Mutation Testing")
+        mutation_test(files, testFiles, output_dir, run_no, test_run_no, error_path)
+        print("**** PHASE 3 COMPLETE ****")
+        print("\n")
     print(f'Results saved in {output_dir}')
 
     return response_history
+
 
 def mixed_modality(files, input_dir, output_dir, error_path, testFiles, input_dir_test, num_of_retries=DEFAULT_NUM_RETRIES):
     #called on sandbox = 1
@@ -275,13 +331,10 @@ def mixed_modality(files, input_dir, output_dir, error_path, testFiles, input_di
     print("**** STARTING MIXED MODALITY TRANSLATION ****")
     run_no = 1
     os.makedirs(output_dir + f'/test_run_{run_no}/')
-    print("1")
     response_history = save_outputs(files, input_dir, output_dir+f'/test_run_{run_no}/', isPhase2=False)
-    print("2")
     if num_of_retries > 0:
-        print("3")
         for testFile in testFiles:
-            shutil.copyfile(input_dir_test + "/" + testFIle + '.py', 'temp/' + testFile + '.py')
+            shutil.copyfile(input_dir_test + "/" + testFile + '.py', 'temp/' + testFile + '.py')
         errors = check_test(testFiles, output_dir, run_no, response_history, error_path)
         if errors:
             print("Errors detected in translated source files. Triggering feedback loop: ")
@@ -303,23 +356,8 @@ def mixed_modality(files, input_dir, output_dir, error_path, testFiles, input_di
     print(f'Results saved in {output_dir}')
     return response_history
 
-
-# if __name__ == "__main__":
-#     #files should only correspond to the files within a given package
-#
-#     files = ["Task", "TaskManager"]
-# #     files = ['AccurateMath', "AccurateMathCalc", "AccurateMathLiteralArrays"]
-#     input_dir = 'LLM-Evaluation/src/main/org/cornell/'
-# #     input_dir = 'commons-math/commons-math-core/src/main/java/org/apache/commons/math4/core/jdkmath/'
-#     output_dir = os.path.join(os.getcwd(), 'translation/', datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
-#     execute_script_path = "translation/execute_java_kc_local.sh"
-#     error_path = "ConvertedCode/converted.txt"
-# #     testFiles = "AccurateMathStrictComparisonTest"
-#     testFiles = ["TaskManagerTest"]
-#     input_dir_test = 'LLM-Evaluation/src/test/java/'
-# #     input_dir_test = "commons-math/commons-math-legacy-core/src/test/java/org/apache/commons/math4/legacy/core/jdkmath/"
-
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Run LLM translation script with specified parameters.")
 
     parser.add_argument('--files', nargs='+', required=True, help='List of files within a given package')
@@ -331,7 +369,7 @@ if __name__ == "__main__":
     parser.add_argument('--sandbox', help='Sandbox codes for running parts of the translation process independently')
 
     args = parser.parse_args()
-
+#
     if args.output_dir is None:
         args.output_dir = os.path.join(os.getcwd(), 'translation/', datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
     if args.execute_script_path is None:
@@ -362,9 +400,8 @@ if __name__ == "__main__":
     #e.g.
     #python3 translation/translate.py --files Task TaskManager --input_dir LLM-Evaluation/src/main/org/cornell/ --test_files TaskManagerTest --input_dir_test LLM-Evaluation/src/test/java/
     #sandbox code:
-    #python3 translation/translate.py --files AccurateMath AccurateMathCalc AccurateMathLiteralArrays  --input_dir commons-math/commons-math-core/src/main/java/org/apache/commons/math4/core/jdkmath/ --test_files AccurateMathTest --input_dir_test ConvertedCode/ --sandbox 1
+    #python3 translation/translate.py --files AccurateMath AccurateMathCalc  --input_dir commons-math/commons-math-core/src/main/java/org/apache/commons/math4/core/jdkmath/ --test_files AccurateMathTest --input_dir_test ConvertedCode/ --sandbox 1
 
 
 #     llm_translate(files, input_dir, output_dir, execute_script_path, error_path, testFiles, input_dir_test)
-
-
+#     mutation_test(args.files, args.test_files, args.output_dir, 3, 3, "ConvertedCode/converted.txt")
